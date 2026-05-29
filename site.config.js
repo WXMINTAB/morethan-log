eac638e77d429089d13e2826ee05c1a8022ae051
WXMINTAB <wxmintab@gmail.com>
2026-05-29 10:44:59 +0900

    Update profile information in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "wxmin.log",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "wxmintab@gmail.com",
+    linkedin: "https://www.linkedin.com/in/kyeongseokmin",
+    x: "https://x.com/wxmintab",
   },
   projects: [
     {
